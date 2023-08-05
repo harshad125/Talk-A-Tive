ca16ad9e13ccb89efc421d0ead4d563ff5a2269f
harshad125 <92792588+harshad125@users.noreply.github.com>
2023-08-05 23:27:50 +0530

    feat:added live video call or meeting feature

--- frontend/chatty/src/component/MeetRoom.js
+++ frontend/chatty/src/component/MeetRoom.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {ZegoUIKitPrebuilt} from "@zegocloud/zego-uikit-prebuilt"
 import { useParams } from 'react-router-dom';
+
+
 function MeetRoom() {
      const {roomid}=useParams();
      console.log(roomid);
-   // const roomid="acdfd1254"
     const mymeet=async(element)=>{
-        const appId=285432938;
-        const serversecret="ba8bc8a4968a29862b1a9c52bfc76421";
+        const appId=Number(process.env.REACT_APP_APPID);
+        const serversecret=process.env.REACT_APP_SERVERSECRET;
         const kitToken=ZegoUIKitPrebuilt.generateKitTokenForTest(appId,serversecret,roomid,Date.now().toString(),"guest meet");
         const zc=ZegoUIKitPrebuilt.create(kitToken);
         zc.joinRoom({
